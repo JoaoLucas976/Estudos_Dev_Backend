99367af753eec0a07151f48ce3dfdd3fe7a823cf
João L. S. Azevedo <55589171+JoaoLucas976@users.noreply.github.com>
2024-11-21 20:55:02 -0300

    Atualizando a variável de entrada para ser do tipo Number

--- JAVASCRIPT/Testes_and_Exerc/Exercicio_Dia_3/app.js
+++ JAVASCRIPT/Testes_and_Exerc/Exercicio_Dia_3/app.js
@@ -1,1 +1,8 @@
-let value = prompt('Enter the product value: ')
+let value = Number(prompt('Enter the product value: '))
+console.log(value)
+
+if (value >= 20){
+    document.getElementById('result').innerHTML = 'Approved'
+} else {
+    document.getElementById('result').innerHTML = 'Denied'
+}
